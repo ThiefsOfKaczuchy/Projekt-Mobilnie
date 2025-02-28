5a2d0877f109cc77739312f4c2a52e13edfaf474
kamerais <kubickimarcin102@gmail.com>
2025-02-28 10:50:31 +0100

    Stworzyłem funkcjonalność dla wszystkich przycisków

--- Podrecznik/app/src/main/java/com/example/podrecznik/MainActivity.kt
+++ Podrecznik/app/src/main/java/com/example/podrecznik/MainActivity.kt
@@ -20,7 +20,26 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, Wprowadznie::class.java)
             startActivity(intent)
         }
-
+        var buttonRasy = findViewById<Button>(R.id.btn_rasy)
+        buttonRasy.setOnClickListener {
+            val intent = Intent(this, Rasy::class.java)
+            startActivity(intent)
+        }
+        var buttonRegiony_lokacje = findViewById<Button>(R.id.btn_regiony_lokacje)
+        buttonRegiony_lokacje.setOnClickListener {
+            val intent = Intent(this, Regiony_lokacje::class.java)
+            startActivity(intent)
+        }
+        var buttonMagie = findViewById<Button>(R.id.btn_magie)
+        buttonMagie.setOnClickListener {
+            val intent = Intent(this, Magie::class.java)
+            startActivity(intent)
+        }
+        var buttonLore = findViewById<Button>(R.id.btn_lore)
+        buttonLore.setOnClickListener {
+            val intent = Intent(this, Lore::class.java)
+            startActivity(intent)
+        }
 
     }
 }
