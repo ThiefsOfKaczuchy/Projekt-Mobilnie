0a5259aec4219838dfb4e8041dc84e573a647134
kamerais <kubickimarcin102@gmail.com>
2025-05-05 17:17:07 +0200

    JPDL działa

--- Podrecznik/app/src/main/java/com/example/podrecznik/Splash.kt
+++ Podrecznik/app/src/main/java/com/example/podrecznik/Splash.kt
@@ -1,19 +1,22 @@
 package com.example.podrecznik
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.appcompat.app.AppCompatActivity
 
+@Suppress("DEPRECATION")
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this@Splash, MainActivity::class.java))
-            finish()
+            startActivity(Intent(this, MainActivity::class.java).apply {
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            })
+        finish()
         }, LOADING_TIME)
     }
 
